4b79795a4f070e40344519a4c682dad6e5dbf47d
firstChetsadakon <first.chetsadakorn@gmail.com>
2024-11-02 22:53:45 +0700

    added core pool size

--- src/main/java/com/dsa/etl/export/th/config/AppConfig.java
+++ src/main/java/com/dsa/etl/export/th/config/AppConfig.java
@@ -25,7 +25,7 @@ public class AppConfig {
     @Bean
     public Executor executorService() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(4);  // Adjust as per your system's capacity
+        executor.setCorePoolSize(10);  // Adjust as per your system's capacity
         executor.setMaxPoolSize(10);
         executor.setQueueCapacity(500);
         executor.setThreadNamePrefix("ETL-");
