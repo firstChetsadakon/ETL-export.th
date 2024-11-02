29833388bd226a4401a6862ce6f73e0a76aa114c
firstChetsadakon <first.chetsadakorn@gmail.com>
2024-11-02 22:56:33 +0700

    @JsonInclude(JsonInclude.Include.NON_NULL)

--- src/main/java/com/dsa/etl/export/th/model/dto/ETLResponse.java
+++ src/main/java/com/dsa/etl/export/th/model/dto/ETLResponse.java
@@ -1,15 +1,18 @@
 package com.dsa.etl.export.th.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ETLResponse {
     private String status;
     private String processId;
-    private long recordsProcessed;
+    //private long recordsProcessed;
     private LocalDateTime timestamp;
     private String message;
 }
